212cddab13116be4ca194248ec545a95807788e7
heart <7362469@qq.com>
2022-12-21 01:53:46 +0800

    fix: fix build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build -t cc-heart-blog:1.0.0 .
 
-docker run -p 80:80 -v ./docs/.vitepress/dist:/opt/blog --name cc-heart-blog -d cc-heart-blog:1.0.0
+docker run -p 80:80 -v /opt/cc-heart-blog/docs/.vitepress/dist:/opt/blog --name cc-heart-blog -d cc-heart-blog:1.0.0
