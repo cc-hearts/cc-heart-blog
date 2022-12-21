8ca5d2db85f00abff442aefac7c2a564c0d024d8
heart <7362469@qq.com>
2022-12-21 09:11:24 +0800

    chore: add build shell

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker rmi -f cc-heart-blog:1.0.0
+
+docker rm -f cc-heart-blog
+
 docker build -t cc-heart-blog:1.0.0 .
 
 docker run -p 80:80 -v /opt/cc-heart-blog/docs/.vitepress/dist:/opt/blog --name cc-heart-blog -d cc-heart-blog:1.0.0
